ae1ddee8c05307641933aa4fd5cedfba2daad691
Erik Lernskog <erik.lernskog@ericsson.com>
2017-11-21 08:51:06 +0100

    Initial commit

---
+++ app/src/main/java/com/lernskog/erik/chatclient/ChatClientActivity.java
@@ -0,0 +1,61 @@
+package com.lernskog.erik.chatclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class ChatClientActivity extends Activity implements View.OnClickListener{
+    public static final String TAG = "ChatClientActivity";
+    private TextView chatlog;
+    private EditText message;
+    private EditText username;
+    private EditText host;
+    private EditText port;
+    private Button send;
+    private Button connect;
+    private Button disconnect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_client);
+        chatlog = (TextView) findViewById(R.id.chatlog);
+        message = (EditText) findViewById(R.id.message);
+        username = (EditText) findViewById(R.id.username);
+        host = (EditText) findViewById(R.id.host);
+        port = (EditText) findViewById(R.id.port);
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(this);
+        connect = (Button) findViewById(R.id.connect);
+        connect.setOnClickListener(this);
+        disconnect = (Button) findViewById(R.id.disconnect);
+        disconnect.setOnClickListener(this);
+        print("onCreate");
+    }
+
+    @Override
+    public void onClick(View v) {
+        // print("onClick " + v.toString());
+        if (v == send) {
+           print("send");
+        } else if (v == connect) {
+            print("connect");
+        } else if (v == disconnect) {
+            print("disconnect");
+        }
+    }
+
+    public void print(final String message) {
+        Log.d(TAG, message);
+        chatlog.post(new Runnable() {
+            @Override
+            public void run() {
+                chatlog.setText(chatlog.getText() + message + "\n");
+            }
+        });
+    }
+}
